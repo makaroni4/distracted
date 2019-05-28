752884bd5d404528d13a3f529935b5000904c2fa
Anatoli Makarevich <makaroni4@gmail.com>
2019-05-29 01:15:56 +0200

    Counting total hits

--- js/app.js
+++ js/app.js
@@ -10,14 +10,38 @@
     document.body.appendChild(anchor);
     document.body.style.display = "block";
 
+    const APP_STORAGE_KEY = "distracted-app";
+
     var app = new Vue({
       el: "#distracted-app",
       data: {
+        totalHits: null
       },
       methods: {
       },
+      mounted() {
+        chrome.storage.local.get([APP_STORAGE_KEY], result => {
+          let appData = result[APP_STORAGE_KEY];
+
+          if(appData) {
+            appData.totalHits += 1;
+          } else {
+            appData = {
+              totalHits: 1
+            }
+          }
+
+          this.totalHits = appData.totalHits;
+
+          chrome.storage.local.set({ [APP_STORAGE_KEY]: appData });
+        });
+      },
       template: `
         <div class="distracted-site-wall">
+          <div class="distracted-site-wall__total-hits" v-if="totalHits">
+            Total hits: {{totalHits}}
+          </div>
+
           <h1>
             This website is blocked by Distracted.
           </h1>
@@ -29,6 +53,7 @@
   const blockedWebsites = [
     "instagram.com",
     "theberrics.com",
+    "www.facebook.com"
   ];
 
   const isBlockedPage = blockedWebsites.some(function(blacklistedUrl) {
