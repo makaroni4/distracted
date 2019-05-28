788015710b3b09ddecf31669b05a00fde8f09ccd
Anatoli Makarevich <makaroni4@gmail.com>
2019-05-28 22:44:43 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+(function() {
+  var initSiteWall = function() {
+    console.log("--> initSiteWall");
+
+    const body = document.querySelector("body");
+    const anchor = document.createElement("div");
+    anchor.id = "distracted-app";
+
+    body.innerHTML = "";
+    document.body.appendChild(anchor);
+    document.body.style.display = "block";
+
+    var app = new Vue({
+      el: "#distracted-app",
+      data: {
+        searchQuery: ""
+      },
+      methods: {
+      },
+      template: `
+        <div class="distracted-site-wall">
+          <h1>
+            This website is blocked by Distracted.
+          </h1>
+        </div>
+      `
+    });
+  }
+
+  const blockedWebsites = [
+    "instagram.com",
+    "theberrics.com",
+  ];
+
+  const isBlockedPage = blockedWebsites.some(function(blacklistedUrl) {
+    return window.location.href.includes(blacklistedUrl);
+  });
+
+  if(isBlockedPage) {
+    document.body.style.display = "none";
+
+    initSiteWall();
+  }
+})();
